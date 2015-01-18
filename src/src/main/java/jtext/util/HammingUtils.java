2326c0fc101c51d690087f9c87ceebcbeb5afe6a
Chrisu <c.bartsch@gmx.at>
2015-01-18 21:08:26 +0100

    Fix bug introduced with change in HammingUtils

--- src/src/main/java/jtext/util/HammingUtils.java
+++ src/src/main/java/jtext/util/HammingUtils.java
@@ -22,13 +22,15 @@ public class HammingUtils {
         }
 
         // If the distance is greater than the threshold, the closest element is too different
-        return minDistance < HAMMING_THRESHOLD ? closestElement : null;
+        // limit threshold to length of input - 1, to avoid finding completely different strings
+        int threshold = Math.min(HAMMING_THRESHOLD, element.length() - 1);
+        return minDistance < threshold ? closestElement : null;
     }
 
     public static int findHammingDistance(String a, String b) {
         boolean firstIsShorter = a.length() <= b.length();
         String shorter = (firstIsShorter ? a : b).toLowerCase();
-        String longer = (!firstIsShorter ? b : a).toLowerCase();
+        String longer = (!firstIsShorter ? a : b).toLowerCase();
         int hammingDistance = 0;
         // Pad String so both have the same length, use 0 so the hamming distance increases for each additional character
         shorter = Strings.padEnd(shorter, longer.length(), (char) 0);
