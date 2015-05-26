411af4e9f7301f84805b2b6a9ff6be09fa6e6306
Findecano <findecano.t@gmail.com>
2015-05-26 11:19:28 +0200

    Add basic controller and wire up controls with angular Add style.css

--- angular/js/jstext.js
+++ angular/js/jstext.js
@@ -26311,3 +26311,16 @@ var minlengthDirective = function() {
 
 var jstext = angular.module("jstext", []);
 
+jstext.controller("GameController", ["$scope", function ($scope) {
+    $scope.location = {
+        name: "Test Location",
+        image: "http://www.theodora.com/maps/new9/time_zones_4.jpg"
+    };
+    $scope.log = ["Some action", "Another action"];
+    $scope.input = "";
+
+    $scope.command = function command() {
+        $scope.log.push($scope.input);
+        $scope.input = "";
+    };
+}]);
--- angular/modules/jstext.js
+++ angular/modules/jstext.js
@@ -3,3 +3,16 @@
 
 var jstext = angular.module("jstext", []);
 
+jstext.controller("GameController", ["$scope", function ($scope) {
+    $scope.location = {
+        name: "Test Location",
+        image: "http://www.theodora.com/maps/new9/time_zones_4.jpg"
+    };
+    $scope.log = ["Some action", "Another action"];
+    $scope.input = "";
+
+    $scope.command = function command() {
+        $scope.log.push($scope.input);
+        $scope.input = "";
+    };
+}]);
