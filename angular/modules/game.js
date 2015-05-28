e70cf4c45de27d58b41258e1a1d241445b41c9c9
Chrisu <c.bartsch@gmx.at>
2015-05-29 01:19:04 +0200

    Implement all game actions except use_with.

--- angular/modules/game.js
+++ angular/modules/game.js
@@ -2,31 +2,30 @@
  * Created by Chrisu on 26.05.2015.
  */
 
+function getOrDefault(value, defaultValue) {
+    return typeof value !== "undefined" ? value : defaultValue;
+}
+
+
 var actions = {
-    enabled: function(gameState, action) {
-        gameState.print("ENABLED NOT IMPLEMENTED YET");
+    enabled: function (gameState, action, entity) {
+        entity.enabled = getOrDefault(action.value, !entity.enabled);
     },
-    remove: function(gameState, action) {
-        gameState.print("REMOVE NOT IMPLEMENTED YET");
+    remove: function (gameState, action, entity) {
+        entity.remove();
     },
-    state: function (gameState, action) {
-        gameState.print("STATE NOT IMPLEMENTED YET");
+    state: function (gameState, action, entity) {
+        entity.state = action.value;
     },
-    take: function(gameState, action) {
-        gameState.print("TAKE NOT IMPLEMENTED YET");
+    take: function (gameState, action, entity) {
+        gameState.inventory[entity.name] = entity;
+        entity.remove();
     },
-    visible: function (gameState, action) {
-        action.targets.forEach(function (targetId) {
-            var entity = gameState.game.allEntities[targetId].model;
-            var value = typeof action.value !== "undefined"
-                ? action.value
-                : !entity.visible;
-            console.log("set visible of", entity, "to", value)
-            entity.visible = value;
-        });
+    visible: function (gameState, action, entity) {
+        entity.visible = getOrDefault(action.value, !entity.visible);
     },
-    win: function(gameState, action) {
-        gameState.print("WIN NOT IMPLEMENTED YET");
+    win: function (gameState, action, entity) {
+        gameState.win();
     }
 };
 
@@ -35,10 +34,9 @@ function EntityInteraction(cmd) {
     this.do = function (gameState, param) {
         var current = gameState.location;
         var entity = param.length == 0
-            ? current.model
-            : current.model.objects[param];
+            ? current : current.objects[param];
 
-        if (!entity || entity.visible === false) {
+        if (!entity || !entity.visible) {
             gameState.print(param + " not found");
         } else {
             var action = entity[this.cmd];
@@ -53,7 +51,11 @@ function EntityInteraction(cmd) {
                     action.do.forEach(function (action) {
                         var actionFunc = actions[action.type];
                         if (actionFunc) {
-                            actionFunc(gameState, action);
+                            var targets = action.targets || [entity.name];
+                            targets.forEach(function (targetId) {
+                                var entity = gameState.allEntities[targetId];
+                                actionFunc(gameState, action, entity);
+                            });
                         }
                     });
                 }
@@ -68,11 +70,11 @@ var interactions = {
     use: new EntityInteraction("use"),
     go: {
         do: function (gameState, param) {
-            var location = gameState.game.allEntities[param];
-            if (location) {
-                if (gameState.location.model.adjacent.indexOf(location.name) >= 0) {
+            var location = gameState.allEntities[param];
+            if (location && location.visible) {
+                if (gameState.location.adjacent.indexOf(location.name) >= 0) {
                     gameState.location = location;
-                    gameState.print("You are not at: " + location.name);
+                    gameState.print("You are now at: " + location.name);
                 } else {
                     gameState.print("Can not go to " + location.name);
                 }
@@ -83,18 +85,72 @@ var interactions = {
     }
 };
 
-function Entity(name, model) {
+function Entity(name, model, parent) {
     this.name = name;
-    this.model = model;
+    this.parent = parent;
+    this.visible = getOrDefault(model.visible, true);
+    this.enabled = getOrDefault(model.enabled, true);
+
+    this.state = model.state || "";
+
+    this.use = model.use || {};
+    this.take = model.take || {};
+    this.look = model.look || {};
+
+    this.objects = model.objects || {};
+
+    //for locations
+    this.adjacent = model.adjacent || [];
+
+    this.remove = function() {
+        if(this.parent) {
+            if(this.name in this.parent.objects) {
+                console.log("removing");
+                delete this.parent.objects[this.name];
+            } else {
+                console.log("remove(), but ", this.name, "not found in", this.parent.name);
+            }
+        } else {
+            console.log("remove(), but no parent for", this.name)
+        }
+    };
+}
+
+function buildEntityMap(model) {
+    var allEntities = {};
+    for (var key in model.locations) {
+        var loc = model.locations[key];
+        var locEntity = new Entity(key, loc, null);
+        allEntities[key] = locEntity;
+
+        for (var objKey in loc.objects) {
+            allEntities[objKey] = new Entity(objKey, loc.objects[objKey], locEntity);
+        }
+    }
+    Object.keys(allEntities).forEach(function (name) {
+        var entity = allEntities[name];
+        Object.keys(entity.objects).forEach(function (objName) {
+            entity.objects[objName] = allEntities[objName];
+        });
+    });
+    return allEntities;
 }
 
 function GameState(game, printer) {
     this.game = game;
     this.print = printer;
 
-    this.location = game.allEntities[game.model.start];
+    this.allEntities = buildEntityMap(game.model);
 
-    this.print(game.model.start_text);
+    this.location = this.allEntities[game.startLocationName];
+
+    this.inventory = {};
+
+    this.print(game.startText);
+
+    this.win = function() {
+        this.print("YOU WIN THE GAME");
+    }
 
     this.enter = function (text) {
         var spacePos = text.indexOf(" ");
@@ -117,17 +173,9 @@ function GameState(game, printer) {
 }
 
 function Game(model) {
+    this.startLocationName = model.start;
+    this.startText = model.start_text;
     this.model = model;
-
-    this.allEntities = {};
-    for (var key in model.locations) {
-        var loc = model.locations[key];
-        this.allEntities[key] = new Entity(key, loc);
-
-        for (var objKey in loc.objects) {
-            this.allEntities[objKey] = new Entity(objKey, loc.objects[objKey]);
-        }
-    }
 
     this.start = function (printer) {
         return new GameState(this, printer);
