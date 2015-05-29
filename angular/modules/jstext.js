a472c2603f285c9b2b05220ea7be10d6111d50b1
Chrisu <c.bartsch@gmx.at>
2015-05-29 04:18:44 +0200

    Add "restart" command to jstext.js.

--- angular/modules/jstext.js
+++ angular/modules/jstext.js
@@ -3,28 +3,43 @@
 var jstext = angular.module("jstext", []);
 
 jstext.controller("GameController", ["$scope", "$http", function ($scope, $http) {
+
+    $scope.restart = function () {
+        $scope.log = [];
+        $scope.gameState = $scope.game.start($scope.print, $scope.onWin);
+        $scope.inventory = $scope.gameState.inventory.objects;
+    };
+
+    $scope.onWin = function () {
+        $scope.restart();
+    };
+
     $http.get("game/test.json").then(function (result) {
         $scope.game = new Game(result.data);
-        $scope.gameState = $scope.game.start($scope.print);
+        $scope.restart();
         $scope.updateLocation();
     });
 
-    $scope.updateLocation = function() {
+    $scope.updateLocation = function () {
         $scope.location = {
             name: $scope.gameState && $scope.gameState.location.name,
             image: "http://www.theodora.com/maps/new9/time_zones_4.jpg"
         };
     };
     $scope.updateLocation();
-    $scope.log = [];
+
     $scope.print = function (text) {
         $scope.log.push({index: $scope.log.length, text: text});
     };
-    $scope.input = "";
 
+    $scope.input = "";
     $scope.command = function command() {
-        $scope.gameState.enter($scope.input);
-        $scope.updateLocation();
+        if ($scope.input === "restart") {
+            $scope.restart();
+        } else {
+            $scope.gameState.enter($scope.input);
+            $scope.updateLocation();
+        }
         $scope.input = "";
     };
 }]);
